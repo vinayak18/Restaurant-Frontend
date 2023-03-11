51ef4b4c8accdaee4d95c5d8983556248ecf8d2c
Sumon Dey <deysumon99@gmail.com>
2023-03-12 01:13:54 +0530

    Menu component UI changes

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+  value = 0;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  handleMinus() {
+    this.value--;
   }
-
+  handlePlus() {
+    this.value++;
+  }
 }
