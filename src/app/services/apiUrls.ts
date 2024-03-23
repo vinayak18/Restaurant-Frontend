31b76fa885ab07883c7940863da20ab431da1ba8
vinayak18 <vinayaksaraf1999@gmail.com>
2024-03-23 19:18:08 +0530

    revert apiUrls.ts file changes

--- src/app/services/apiUrls.ts
+++ src/app/services/apiUrls.ts
@@ -1,7 +1,5 @@
-const productserviceUrl = 'https://restaurant-app.up.railway.app/api/v1/productservice';
-//'/productserviceurl/api/v1/productservice';
-const userserviceUrl = 'https://restaurant-app.up.railway.app/api/v1/userservice';
-// '/userserviceurl/api/v1/userservice';
+const productserviceUrl = '/productserviceurl/api/v1/productservice';
+const userserviceUrl = '/userserviceurl/api/v1/userservice';
 const feedbackserviceUrl = '/feedbackserviceurl/api/v1/feedbackservice';
 const messageserviceUrl = '/messageserviceurl/api/v1/messageservice';
 
